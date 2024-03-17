7cf47cfe33317be7aa47e19b14059e232d30b34c
Aditya Pant <123084564+Aadi1235@users.noreply.github.com>
2024-03-17 20:48:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ window.addEventListener ("scroll", function() {
   header.classList.toggle ("sticky",  window.scrollY > 0);
 });
 
-let menu = document.querySelector('menu-icon');
-let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-};
-
-window.onscroll  = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('open');
-}; 
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0"
+}
+function closemenu() {
+    sidemenu.style.right = "-200px"
+}
